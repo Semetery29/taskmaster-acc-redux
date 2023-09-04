e38d58a5e03a847ca309032e6226f34f7ab38c3b
Semetery29 <mdazizulhakimsuny@gmail.com>
2023-09-04 11:48:35 +0600

    first commit

---
+++ src/redux/features/tasks/taskSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  tasks: [],
+};
+
+const tasksSlice = createSlice({
+  name: "taskSlice",
+  initialState,
+  reducers: {},
+});
+
+
+export default tasksSlice.reducer;
